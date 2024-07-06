43a25c625a8c6b402388ebad4b2c43336eff79cf
Matthias Schmitz <25723848+lksae@users.noreply.github.com>
2024-07-06 19:56:13 +0200

    finish assignment

--- lib/event_manager_time_and_date.rb
+++ lib/event_manager_time_and_date.rb
@@ -1,6 +1,6 @@
 require 'csv'
-require 'time'
 require 'date'
+require 'erb'
 
 puts 'EventManager Assignments initialized.'
 
@@ -10,8 +10,38 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
-contents.each do |row|
-  # time = DateTime.parse(row[:regdate])
-  time = DateTime.strptime(row[:regdate], '%m/%d/%y %H:%M')
-  puts "#{time.hour}"
+hours = Hash.new(0)
+days = Hash.new(0)
+
+def get_hour_of_reg_day(time, hours)
+  time = DateTime.strptime(time, '%m/%d/%y %H:%M').hour
+  hours[time] += 1
 end
+
+def print_table_of_hours_and_weekdays(hours, days)
+  hours = hours.sort.to_h
+  days = days.sort.to_h
+  hour_and_day_template = File.read('hours_and_days_table.erb')
+  erb_template = ERB.new hour_and_day_template
+  hour_and_day_table = erb_template.result(binding)
+
+  filename = 'hours_and_days_table.html'
+
+  File.open(filename, 'w') do |file|
+    file.puts hour_and_day_table
+  end
+end
+
+def get_weekday_of_reg_day(day, days)
+  weekday = Date.strptime(day, '%m/%d/%y').wday
+  # puts date
+  days[weekday] += 1
+end
+
+contents.each do |row|
+  get_hour_of_reg_day(row[:regdate], hours)
+  get_weekday_of_reg_day(row[:regdate], days)
+end
+
+print_table_of_hours_and_weekdays(hours, days)
+puts 'table printed'

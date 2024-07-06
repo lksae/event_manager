b985c86351757517068821c26527b9a053d2e2e5
Matthias Schmitz <25723848+lksae@users.noreply.github.com>
2024-07-06 13:04:28 +0200

    secret something

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -8,7 +8,7 @@ end
 
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-  civic_info.key = File.read('secret.txt').strip #'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
+  civic_info.key = File.read('secret.txt').strip 
 
   begin
     civic_info.representative_info_by_address(
